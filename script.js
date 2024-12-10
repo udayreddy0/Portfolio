cfdcbd68100673fbb3b80ed505bc0630fde501f8
udayreddy0 <141111269+udayreddy0@users.noreply.github.com>
2024-12-10 10:39:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ function cancel(){
     const navbar = document.querySelector(".dropdown")
     navbar.style.transform = "translateY(-500px)"
 }
-// Typewriter Effect
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "YOUTUBER"
 ]
 let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
