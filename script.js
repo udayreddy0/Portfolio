add417294fe446f9b904554a1cbe27a630c0b85a
udayreddy0 <141111269+udayreddy0@users.noreply.github.com>
2024-12-10 10:08:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".navbar a");
-  links.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      link.style.color = "#FF5733";
-    });
-    link.addEventListener("mouseout", () => {
-      link.style.color = "white";
-    });
-  });
-});
 
